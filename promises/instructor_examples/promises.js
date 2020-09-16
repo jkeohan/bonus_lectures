5b460db2b7d77555c04b713bcce5f7d95ff986dd
Joe Keohan <joe.keohan@generalassemb.ly>
2020-09-16 16:25:40 -0400

    Update promises.js

--- promises/instructor_examples/promises.js
+++ promises/instructor_examples/promises.js
@@ -1,8 +1,14 @@
+//////////////////////////
+// PROMISE PENDING
+//////////////////////////
 // const promisePending = new Promise( (resolve,reject) => {
   
 // })
-
 // console.log('promisePending - ', promisePending)
+
+//////////////////////////
+// PROMISE RESOLVED
+//////////////////////////
 
 // let promiseResolved = new Promise( (resolve, reject) => {
 //   setTimeout(() => resolve({key:'data'}), 3000)
@@ -15,6 +21,10 @@
 //   console.log('val is:', val) 
 // })
 // console.log('this runs after .then')
+
+//////////////////////////
+// PROMISE REJECTED
+//////////////////////////
 
 // let promiseRejected = new Promise( (resolve, reject) => {
 //   setTimeout( () => reject('you have been rejected'), 3000)
@@ -29,6 +39,11 @@
 // // .finally will run regardless of resolve/reject
 //   .finally( () => console.log('this is finally'))
 
+
+//////////////////////////
+// WORKING WITH RESOLVED PROMISES
+//////////////////////////
+
 // let promise = new Promise( (resolve, reject) => {
 //   resolve(1)
 // })
@@ -41,6 +56,10 @@
 //   console.log(('second then:', val))
 // })
 
+//////////////////////////
+// $.AJAX() EXAMPLE OF PROMISES
+//////////////////////////
+
 // $.ajax(googleUrl)
   // .then((val => {
   //   // format the data
@@ -50,24 +69,28 @@
   //   // pass the data to the renderCards function
   // })
 
-  // in unit 2 we will be using a new api tool to fetch data called: fetch
+//////////////////////////
+// RECREATE FETCH TOOL
+//////////////////////////
 
-  const fetcher = (url) => {
-    return new Promise( (resolve, reject) => { 
-      let xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-        if (xhr.status >= 200 && xhr.status < 300) {
-          resolve(JSON.parse(xhr.responseText));
-          // console.log(data)
-        } else {
-          reject('The request failed!');
-        }
-      };
-      xhr.open('GET', url);
-      xhr.send();
-    })
-}
+// in unit 2 we will be using a new API tool to retrieve data called: fetch
 
-fetcher('https://pokeapi.co/api/v2/pokemon')
-.then(data => data.results.map(d => d.name))
-.then(data => console.log('data', data))
+//   const fetcher = (url) => {
+//     return new Promise( (resolve, reject) => { 
+//       let xhr = new XMLHttpRequest();
+//       xhr.onload = function () {
+//         if (xhr.status >= 200 && xhr.status < 300) {
+//           resolve(JSON.parse(xhr.responseText));
+//           // console.log(data)
+//         } else {
+//           reject('The request failed!');
+//         }
+//       };
+//       xhr.open('GET', url);
+//       xhr.send();
+//     })
+// }
+
+// fetcher('https://pokeapi.co/api/v2/pokemon')
+// .then(data => data.results.map(d => d.name))
+// .then(data => console.log('data', data))
